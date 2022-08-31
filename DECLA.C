2c65c2eb02e5e231c1b9dd8ca155c2ccf58c1c49
janvijadav <110669936+janvijadav@users.noreply.github.com>
2022-08-31 16:34:50 +0530

    Add files via upload

---
+++ DECLA.C
@@ -0,0 +1,18 @@
+#include<stdio.h>         //standerd input output
+#include<conio.h>         //console input output
+//define section
+#define PI 3.14           //symfolic constant
+//global declaration sction
+int a=20;
+void main()//main faction action
+{
+     //declaration part
+     int b=45;
+     //executable part
+     clrscr();
+     printf("first program of c");
+     printf("\n a=%d",a);
+     printf("\n b=%d",b);
+     printf("\n PI=%f",PI);
+     getch();
+}
---
+++ PRISM.C
@@ -0,0 +1,15 @@
+//volume of prism
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+     double a,b,vopr;
+     clrscr();
+     printf("enter value of v",vopr);
+     scanf("%lf %lf",&a,&b);
+     vopr=a*b;
+     printf("vopr i %lf",vopr);
+     getch();
+}
