65eb97075ad35ea6e20c6c3ae5d3cd6b41f9f6aa
janvijadav <110669936+janvijadav@users.noreply.github.com>
2022-08-31 17:18:23 +0530

    Add files via upload

---
+++ ADD.C
@@ -0,0 +1,15 @@
+//write that program that print sum of the nos.
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+      int a,b,c;
+      clrscr();
+      printf("enter two nos:");
+      scanf("%d %d",&a, &b);
+      c=a+b;
+      printf("\n %d+%d=%d",a,b,c);
+      getch();
+}
