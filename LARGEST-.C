a38490edd78150dde5d7b9c9ca788f1e7bef90e4
janvijadav <110669936+janvijadav@users.noreply.github.com>
2022-09-01 17:54:08 +0530

    Add files via upload

---
+++ LARGEST-.C
@@ -0,0 +1,18 @@
+//if else largest
+
+void main()
+{
+     int a,b;
+     clrscr();
+     printf("enter two nos.:");
+     scanf("%d %d",&a,&b);
+     if(a>b)
+     {
+	  printf("\n %d is largest",a);
+     }
+     else
+     {
+	 printf("\n %d is largest",b);
+     }
+     getch();
+}
