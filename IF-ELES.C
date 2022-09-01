2a001bcc2d7899a0deab7369c995cb0c35b3b674
janvijadav <110669936+janvijadav@users.noreply.github.com>
2022-09-01 17:42:18 +0530

    Add files via upload

---
+++ IF-ELES.C
@@ -0,0 +1,18 @@
+//if eles
+
+void main()
+{
+       int a,b;
+       clrscr();
+       printf("enter two nos.:"),
+       scanf("%d %d",&a,&b);
+       if(a<b)
+       {
+	    printf("\n %d is smaller",a);
+       }
+       else
+       {
+	    printf("\n %d is smaller",b);
+       }
+       getch() ;
+}
