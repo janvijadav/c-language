69e807728509741c9cc42c360f28b1fcee3ec103
janvijadav <110669936+janvijadav@users.noreply.github.com>
2022-09-15 13:13:53 +0530

    Add files via upload

---
+++ #@.C
@@ -0,0 +1,22 @@
+
+// # @.....
+
+void main()
+{
+	int n,i,j;
+	clrscr();
+	printf("Enter n:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=i;j>=1;j--)
+		{
+			if(j%2==0)
+				printf("@");
+			else
+				printf("#");
+		}
+		printf("\n");
+	}
+	getch();
+}
